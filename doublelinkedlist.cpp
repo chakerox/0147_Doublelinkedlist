99b1531b4b00074885487b845618561b7b186f12
chakerox <chakerox@gmail.com>
2026-04-30 13:12:03 +0700

    membuat switch case

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -227,4 +227,31 @@ int main()
         cout << "Enter your choice: ";
 
         cin >> choice;
-
+         switch (choice)
+        {
+        case '1':
+            list.addNode();
+            break;
+        case '2':
+            list.hapus();
+            break;
+        case '3':
+            list.traverse();
+            break;
+        case '4':
+            list.revtraverse();
+            break;
+        case '5':
+            list.searchData();
+            break;
+        case '6':
+            return 0;
+        default:
+            cout << "Invalid option\n";
+        }
+        cout << "\nPress Enter to continue...";
+        cin.ignore();
+        cin.get();
+        system("clear");
+    } while (choice != '6');
+}
